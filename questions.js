038194b0bcede517e6e898ef89e364ddfde55ae6
BondT1 <tombond248@gmail.com>
2023-06-29 19:23:11 +0100

    started questions.js code

---
+++ questions.js
@@ -0,0 +1,18 @@
+const MainQuestions = [
+    {
+        type: 'list',
+        name: 'option',
+        message: 'What do you want to do?',
+        choices: [
+            { value: 'view_departments', name: "view all departments" },
+            { value: 'view_roles', name: "view all roles"},
+            { value: 'view_employees', name: "view all employees"},
+            { value: 'add_department', name: "add a department"},
+            { value: 'add_role', name: "add a role"},
+            { value: 'add_employee', name: "add an employee"},
+            { value: 'update_role', name: "update an employee role"},
+        ],
+
+    },
+]
+
