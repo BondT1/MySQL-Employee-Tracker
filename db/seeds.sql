bf6ac83bb720b151f7e87e2973786104f2668fa7
BondT1 <tombond248@gmail.com>
2023-06-29 20:43:30 +0100

    added roles seed.sql

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INTO department (id, name) VALUES (1, 'Sales');
+INTO department (id, name) VALUES (2, 'Engineering');
+INTO department (id, name) VALUES (3, 'Finance');
+INTO department (id, name) VALUES (4, 'Legal');
+
+INTO role (id, title, salary, department_id) VALUES (1, 'Sales Lead', 100000, 1);
+INTO role (id, title, salary, department_id) VALUES (2, 'Salesperson', 80000, 1);
+INTO role (id, title, salary, department_id) VALUES (3, 'Lead Engineer', 150000, 2);
+INTO role (id, title, salary, department_id) VALUES (4, 'Software Engineer', 120000, 2);
+INTO role (id, title, salary, department_id) VALUES (5, 'Account Manager', 160000, 3);
+INTO role (id, title, salary, department_id) VALUES (6, 'Accountant', 125000, 3);
+INTO role (id, title, salary, department_id) VALUES (7, 'Legal Team Lead', 250000, 4);
+INTO role (id, title, salary, department_id) VALUES (8, 'Lawyer', 190000, 4);
+
+
