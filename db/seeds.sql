bd095c51d95cdc4ee571a7900b010dd225977625
BondT1 <tombond248@gmail.com>
2023-06-29 20:53:12 +0100

    database info on seed.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,4 +12,12 @@ INTO role (id, title, salary, department_id) VALUES (6, 'Accountant', 125000, 3)
 INTO role (id, title, salary, department_id) VALUES (7, 'Legal Team Lead', 250000, 4);
 INTO role (id, title, salary, department_id) VALUES (8, 'Lawyer', 190000, 4);
 
+INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, 'John', 'Doe', 1, NULL);
+INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (2, 'Mike', 'Chan', 2, 1);
+INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (3, 'Ashley', 'Rodriguez', 3, NULL);
+INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (4, 'Kevin', 'Tupik', 4, 3);
+INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (5, 'Kunal', 'Singh', 5, NULL);
+INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (6, 'Malia', 'Brown', 6, 5);
+INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (7, 'Sarah', 'Lourd', 7, NULL);
+INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (8, 'Tom', 'Allen', 8, 7);
 
