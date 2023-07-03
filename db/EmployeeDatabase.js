784cdfd47213c7da3bbcacd65fc66efc7572761d
BondT1 <tombond248@gmail.com>
2023-07-03 20:27:28 +0100

    getEmployees function

--- db/EmployeeDatabase.js
+++ db/EmployeeDatabase.js
@@ -26,4 +26,31 @@ class EmployeeDatabase extends Database {
             });
         });
     }
+
+    getEmployees() {
+        return new Promise((resolve, reject) => {
+            this.db.query(
+                `SELECT
+                    employee.id,
+                    CONCAT(employee.first_name, ' ', employee.last_name) as name,
+                    role.title as role_title,
+                    role.salary as role_salary,
+                    dpeartment.name as department_name,
+                    if(CONCAT(manager.first_name, ' ', manager.last_name) IS NULL , '', CONCAT(manager.first_name, ' ', manager.last_name))
+                    
+                FROM employee
+                    INNER JOIN role ON employee.role_id = role.id
+                    INNER JOIN department ON role.department_id = department.id
+                    LEFT JOIN employee as manager ON employee.manager_id = manager.id`
+                , (err, results) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(results);
+                });
+            
+        });
+    }
+
+    
 }
